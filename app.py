9bbb006b54d8726d20499540c1994d62527e7fbf
OnurKorkmaz <korkmazo1903@gmail.com>
2023-11-06 23:11:33 +0300

    revision update

--- app.py
+++ app.py
@@ -31,6 +31,51 @@ st.header("How many collisions occur during a given time of day?")
 hour = st.sidebar("Hour to look at", 0, 23)
 data =data[data["data/time"].dt.hour == hour]
 
+st.markdown("Vehicle collesions between %i:00 and%i:00" % (hour, (hour + 1)%24))
+midpoint = np.average(data["latitude"]),np.average(data["longitude"])
+
+
+
+st.write(pdk.Deck(
+    map_style = "mapbox://styles/mapbox/light-v9",
+    initial_view_state ={
+        "latitude": midpoint[0],
+        "longitude": midpoint[1],
+        "zoom" : 11,
+        "pitch": 50,
+    },
+    layers =[
+        pdk.Layer(
+        "HexagonLayer",
+        data = data[["date/time","latitude","longitude"]],
+        get_position = ["logitude","latitude"],
+        radius = 100,
+        extruded = True,
+        pickable = True,
+        elevation_scale =4,
+        elevation_range= [0,1000],
+
+        ),
+    ]
+))
+
+st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour,(hour+1) %24))
+filtered = data[
+    (data["date/time"].dt.hour>= hour) & (data["date/time"].dt.hour < (hour+1))
+
+]
+hist = np.histogram(filtered["date/time"].dt.minute, bins= 60,range=(0,60))[0]
+chart_Data = pd.DataFrame({"minute": range(60),"crashes":hist})
+fig= px.bar(chart_Data,x="minute", y="crashes",hover_data=["minute","crashes"], height=400 )
+st.write(fig)
+
+
+
+
+
+
+
+
 
 
 
